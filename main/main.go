19f089928d42aa13002267026a37b99cdb484c4f
Alexandru Grigor <alex.grigor92@gmail.com>
2018-02-26 05:17:19 -0800

    step7

--- main/main.go
+++ main/main.go
@@ -5,14 +5,13 @@ import (
 	"goworkshop/importer"
 	"goworkshop/model"
 	"goworkshop/web"
-	"goworkshop/persistence"
 )
 
 func main() {
-	persistence.InitDB()
 	model.Authors = importer.ImportAuthors()
 	fmt.Printf("Imported authors are: %s\n", model.Authors)
 	model.Books = importer.ImportBooks()
 	fmt.Printf("Imported books are: %s\n", model.Books)
 	web.StartServer()
 }
+
